16c70e0084eeb3c1eef448faa79f56cb16310caf
talyamatt <talya.matt2@gmail.com>
2025-02-17 18:13:51 +0200

    created print hello world

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -1,4 +1,4 @@
 # THIS CODE IS WRONG, FIX IT AND ADD NEW CODE
-name = input("HeLo WAT Name you")
-b_size = int(input(f"{name}, please choose b size"))
-print(f"{name}, the board size is: {b_size}, number of mines is: anfkja")
+name = input("Hello, whats your name")
+board_size = int(input("{name},please choose board size"))
+print("{name}, the board size is: {board_size}, number of mines is: {board_size} ")
