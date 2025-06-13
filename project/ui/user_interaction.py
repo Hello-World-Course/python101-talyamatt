0a72b1fb4c5cac526ee2eadff49ad2b742bb5b18
talyamatt <talya.matt2@gmail.com>
2025-06-13 18:43:59 +0300

    created print hello world

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -1,7 +1,7 @@
-name = input("Hello, whats your name\ninput:>")
-board_size_str = input( f"{name}, please choose board size\ninput:>")
+name = input("Hello, whats your name")
+board_size_str = input( f"{name}, please choose board size")
 board_size = int(board_size_str)
-number_of_mines_str = input( f"{name}, for board size {board_size}, choose number of mines to allocate\ninput:>" )
+number_of_mines_str = input( f"{name}, for board size {board_size}, choose number of mines to allocate" )
 number_of_mines = int(number_of_mines_str)
 print(f"{name}, the board size is: {board_size}, number of mines is: {number_of_mines}. ENJOY!")
 
