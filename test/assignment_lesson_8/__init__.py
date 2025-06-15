754cfea78340e8bddcea93ca73cd32de565ef1b8
talyamatt <talya.matt2@gmail.com>
2025-06-15 15:33:19 +0300

    created print hello world

---
+++ test/assignment_lesson_8/__init__.py
@@ -0,0 +1,39 @@
+name = None
+board_size = None
+number_of_mines = None
+
+player_name_input = input("Hello, whats your name")
+
+if len(player_name_input) > 2:
+    name = player_name_input
+else:
+    print("Your name is too short")
+
+if name is not None:
+    board_size_input = input(f"{name}, please choose board size")
+
+    temp_board_size = int(board_size_input)
+
+    if 0 < temp_board_size < 26:
+        board_size = temp_board_size
+    else:
+        print(f"{name}, you have entered illegal board size")
+
+if name is not None and board_size is not None:
+    number_of_mines_input = input(f"{name}, for board size {board_size}, choose number of mines to allocate")
+
+    temp_number_of_mines = int(number_of_mines_input)
+
+    max_mines_allowed = (board_size * board_size) // 2
+
+    if 0 < temp_number_of_mines <= max_mines_allowed:
+        number_of_mines = temp_number_of_mines
+    else:
+        print(f"{name}, you have entered illegal number of mines")
+
+if name is not None and board_size is not None and number_of_mines is not None:
+    print(f"{name}, the board size is: {board_size}, number of mines is: {number_of_mines}. ENJOY!")
+
+
+
+
