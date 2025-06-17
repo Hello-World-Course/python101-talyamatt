02182cc687d0ca54a84f51a96dd418acfbbb91ef
talyamatt <talya.matt2@gmail.com>
2025-06-17 22:46:44 +0300

    created print hello world

--- test/assignment_lesson_8/__init__.py
+++ test/assignment_lesson_8/__init__.py
@@ -1,43 +1,55 @@
+get_user_parameters()
+
 name = None
 board_size = None
 number_of_mines = None
+
 
 def get_user_parameters():
     global name
     global board_size
     global number_of_mines
 
-    player_name_input = input("Hello, whats your name")
+    # Get player name with validation loop
+    while name is None:
+        player_name_input = input("Hello, what's your name? ")
 
-    if len(player_name_input) > 2:
-        name = player_name_input
-    else:
-        print("Your name is too short")
-        return
+        if len(player_name_input.strip()) > 2:
+            name = player_name_input.strip()
+        else:
+            print("Your name is too short (must be more than 2 characters)")
 
-    board_size_input = input(f"{name}, please choose board size")
+    # Get board size with validation loop
+    while board_size is None:
+        try:
+            board_size_input = input(f"{name}, please choose board size (1-25): ")
+            temp_board_size = int(board_size_input)
 
-    temp_board_size = int(board_size_input)
+            if 0 < temp_board_size < 26:
+                board_size = temp_board_size
+            else:
+                print(f"{name}, you have entered illegal board size. Please enter a number between 1 and 25.")
+        except ValueError:
+            print("Please enter a valid number.")
 
-    if 0 < temp_board_size < 26:
-        board_size = temp_board_size
-    else:
-        print(f"{name}, you have entered illegal board size")
-        return
+    # Get number of mines with validation loop
+    while number_of_mines is None:
+        try:
+            max_mines_allowed = (board_size * board_size) // 2
+            number_of_mines_input = input(
+                f"{name}, for board size {board_size}x{board_size}, choose number of mines (1-{max_mines_allowed}): ")
+            temp_number_of_mines = int(number_of_mines_input)
 
-    number_of_mines_input = input(f"{name}, for board size {board_size}, choose number of mines to allocate")
+            if 0 < temp_number_of_mines <= max_mines_allowed:
+                number_of_mines = temp_number_of_mines
+            else:
+                print(
+                    f"{name}, you have entered illegal number of mines. Please enter a number between 1 and {max_mines_allowed}.")
+        except ValueError:
+            print("Please enter a valid number.")
 
-    temp_number_of_mines = int(number_of_mines_input)
+    print(f"{name}, the board size is: {board_size}x{board_size}, number of mines is: {number_of_mines}. ENJOY!")
 
-    max_mines_allowed = (board_size * board_size) // 2
 
-    if 0 < temp_number_of_mines <= max_mines_allowed:
-        number_of_mines = temp_number_of_mines
-    else:
-        print(f"{name}, you have entered illegal number of mines")
-        return
-
-    if name is not None and board_size is not None and number_of_mines is not None:
-        print(f"{name}, the board size is: {board_size}, number of mines is: {number_of_mines}. ENJOY!")
-
+# Call the function
 get_user_parameters()
